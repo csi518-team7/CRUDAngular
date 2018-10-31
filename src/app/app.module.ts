a75229c43c8bd5753146a7ca14ba934cc333ec5e
EricYang3721 <xinyang3721@gmail.com>
2018-10-31 11:07:13 -0400

    seperated and beatified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,20 +7,36 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
-
+import { RouterModule } from "@angular/router";
 import { TodoService } from './todo.service';
 import { DlDateTimePickerDateModule } from 'angular-bootstrap-datetimepicker';
+import { AdminFrontComponent } from './admin-front/admin-front.component';
+import { EventListPageComponent } from './event-list-page/event-list-page.component';
+import { CreateEventComponent } from './create-event/create-event.component';
+import { GlobalHeaderComponent } from './global-header/global-header.component';
+
+
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    AdminFrontComponent,
+    EventListPageComponent,
+    CreateEventComponent,
+    GlobalHeaderComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpModule,
     DlDateTimePickerDateModule,
+    RouterModule.forRoot([
+      { path: "", component: AdminFrontComponent }, 
+      { path: "eventList", component: EventListPageComponent },  
+      { path: "createEvent", component: CreateEventComponent },  
+    ])
   ],
   providers: [TodoService, FormsModule],
   bootstrap: [AppComponent]
